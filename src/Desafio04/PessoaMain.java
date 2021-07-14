e0283e6b9677f93a63469183ad3ed237f4ed6842
Willian Dagostin Bittencourt <williandb@gmail.com>
2021-07-14 14:03:12 -0300

    texto

--- src/Desafio04/PessoaMain.java
+++ src/Desafio04/PessoaMain.java
@@ -2,7 +2,7 @@ package Desafio04;
 
 public class PessoaMain {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario(1,"Willian Dagostin Bittencourt", "Rua Teste", "48 99914-8456", "04360096925", "3550127","M", 4527.74, 4000.0);
+        Funcionario funcionario = new Funcionario(1,"Willian Dagostin Bittencourt", "Rua Teste", "48 99914-8***", "01230045625", "3550447","M", 4527.74, 4000.0);
         PessoaJuridica pessoaJuridica = new PessoaJuridica(2,"Fulano de Tal", "Rua Teste", "0123456789","00456865000752","123456","501234");
 
         System.out.println(funcionario.getDocumentoPrincipal());
